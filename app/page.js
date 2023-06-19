240ab5793ac5fdd2fba58ec511c9d64f37f761a9
anupam <puspaanupam@gmail.com>
2023-06-19 23:40:21 +0545

    profile page added

--- app/page.js
+++ app/page.js
@@ -2,7 +2,16 @@
 export default function Home() {
   return (
     <div className="">
-    Home page  
+      <div className="hero min-h-screen bg-base-200">
+        <div className="hero-content flex-col lg:flex-row-reverse">
+          <img src="https://external-content.duckduckgo.com/iu/?u=https%3A%2F%2Ftargettrend.com%2Fwp-content%2Fuploads%2F2020%2F12%2Flearn-JavaScript-online-free-1-1-1536x1024.jpeg&f=1&nofb=1&ipt=6620c0005b9c8e7589d0c1a3a2b0b02b3e97eb3a1d4fa8d8d7a2f94928a7da12&ipo=images" className="max-w-sm rounded-lg shadow-2xl" />
+          <div>
+            <h1 className="text-5xl font-bold">Next Learn Blog</h1>
+            <p className="py-6">Provident cupiditate voluptatem et in. Quaerat fugiat ut assumenda excepturi exercitationem quasi. In deleniti eaque aut repudiandae et a id nisi.</p>
+            <button className="btn btn-primary">Get Started</button>
+          </div>
+        </div>
+      </div>
     </div>
   )
 }
--- components/NavBar.jsx
+++ components/NavBar.jsx
@@ -5,7 +5,7 @@ const NavBar = () => {
         <div>
             <div className="navbar bg-base-100">
                 <div className="flex-1 ">
-                    <a className="btn btn-ghost normal-case text-xl">daisyUI</a>
+                    <a className="btn btn-ghost normal-case text-xl">next_learn</a>
                 </div>
                 <div className="flex-none gap-2">
                     <div className="form-control">
@@ -14,7 +14,7 @@ const NavBar = () => {
                     <div className="dropdown dropdown-end">
                         <label tabIndex={0} className="btn btn-ghost btn-circle avatar">
                             <div className="w-10 rounded-full">
-                                <img src="/images/stock/photo-1534528741775-53994a69daeb.jpg" />
+                                <img src="https://external-content.duckduckgo.com/iu/?u=https%3A%2F%2Fpre00.deviantart.net%2F212e%2Fth%2Fpre%2Fi%2F2015%2F204%2F0%2F7%2Fwater_tribe_jon_snow_by_pip11-d92jkl3.jpg&f=1&nofb=1&ipt=36b2508c586845db6d21ad32240e72c305e5d6a698a5ea2fb1b53cc3d28ffa98&ipo=images" />
                             </div>
                         </label>
                         <ul tabIndex={0} className="mt-3 p-2 shadow menu menu-sm dropdown-content bg-base-100 rounded-box w-52">
